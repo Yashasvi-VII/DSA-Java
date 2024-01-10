87493d2b7b160e318c55d2999860ae3f09ff5927
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-10 15:35:50 +0530

    data types practice done

--- DataTypes/src/Main.java
+++ DataTypes/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int myNumber = 1;
+        System.out.println(myNumber);
+
+        boolean isVeg = false;
+        System.out.println(isVeg);
+
+        float myFloat = 0.2f;
+        System.out.println(myFloat);
+
+        double myDouble = 2.9;
+        System.out.println(myDouble);
+
+        String myName = "xyz";
+        System.out.println(myName);
     }
 }
