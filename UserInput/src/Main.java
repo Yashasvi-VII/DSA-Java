34d4878641dca8cea4826aa469463807e676fd0a
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-10 21:37:02 +0530

    User input demo code done

--- UserInput/src/Main.java
+++ UserInput/src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = input.nextLine();
+        System.out.println("Welcome " + name);
     }
 }
