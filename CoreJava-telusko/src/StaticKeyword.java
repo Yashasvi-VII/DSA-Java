d87ea8ff5b7fd948fb0f7650325f869394d643eb
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-24 00:19:48 +0530

    Calling static variables with classNames

--- CoreJava-telusko/src/StaticKeyword.java
+++ CoreJava-telusko/src/StaticKeyword.java
@@ -19,14 +19,15 @@ public class StaticKeyword {
         Mobile obj1 = new Mobile();
         obj1.brand = "Apple";
         obj1.price = 100000;
-        obj1.name = "Smartphone";
-
+//        obj1.name = "Smartphone";
+        //static variable should be called with their classNames
+Mobile.name="Smartphone";
         Mobile obj2 = new Mobile();
         obj2.brand = "Samsung";
         obj2.price = 80000;
         obj2.name = "Smartphone";
 
-        obj1.name="Phone";
+//        obj1.name="Phone";
 
         obj1.show();
         obj2.show();
