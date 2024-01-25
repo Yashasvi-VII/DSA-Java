ce4f99396a3ee2f44b78eea568a07ffe8b32182f
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-25 10:21:42 +0530

    Fixed extra spacing and formated the code

--- CoreJava-telusko/src/Constructor.java
+++ CoreJava-telusko/src/Constructor.java
@@ -1,16 +1,17 @@
-class Humandemo {
+class HumanDemo {
     private int age;
     private String name;
 
-//    constructor syntax is similar to method, but only diffrence is constructor
-//            does not have return type.And constructor name should be same as class name
-    public Humandemo(){
+    // constructor syntax is similar to method, but only difference is constructor
+    // does not have return type.And constructor name should be same as class name
+    public HumanDemo() {
+
         // Every time we create an object, constructor will be called.
         System.out.println("in constructor");
 
         // we can assign values to instance variables in constructor
-        age=11;
-        name="yashasvi";
+        age = 11;
+        name = "yashasvi";
 
         //There is a convention when ever you are doing an operation
         // like assigning a value or calculation always do it in method
@@ -18,9 +19,7 @@ class Humandemo {
 
         //When we are making a connection with the dataBase or network
         // we can write the connection in the constructor
-
     }
-
 
     //Getter method to fetch age
     public int getAge() {
@@ -33,25 +32,25 @@ class Humandemo {
     }
 
     //Setter method to set age
-    public void setAge(int age){
-        //this refers to current object who is calling the set age
-        this.age=age;
+    public void setAge(int age) {
+
+        // this refers to current object who is calling the set age
+        this.age = age;
     }
 
     //Setter method to set name
-    public void setName(String name){
-        this.name=name;
+    public void setName(String name) {
+        this.name = name;
     }
 }
+
 public class Constructor {
     public static void main(String[] args) {
-        Humandemo obj = new Humandemo();
-//        obj.age = 11;
-//        obj.name = "yashasvi";
+        HumanDemo obj = new HumanDemo();
 
-//        obj.setAge(11);
-//        obj.setName("yashasvi");
-        System.out.println(obj.getName() + " "+ obj.getAge());
+        // obj.setAge(11);
+        // obj.setName("yashasvi");
+        System.out.println(obj.getName() + " " + obj.getAge());
     }
 }
 
