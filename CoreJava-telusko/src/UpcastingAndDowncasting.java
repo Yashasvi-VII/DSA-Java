ac50d016b97fac0a584d66e1346470b9767f8b85
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-27 12:38:57 +0530

    Type casting demo code

---
+++ CoreJava-telusko/src/UpcastingAndDowncasting.java
@@ -0,0 +1,11 @@
+public class UpcastingAndDowncasting {
+    public static void main(String[] args) {
+
+        double d = 4.5;
+        //Type casting from double to int
+        int i = (int) d;
+
+        System.out.println(d);
+    }
+
+}
