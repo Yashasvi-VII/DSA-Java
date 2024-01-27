3e366222ea3878dd043769ec27dfcf7543a45d82
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-27 12:54:22 +0530

    Up casting

--- CoreJava-telusko/src/UpcastingAndDowncasting.java
+++ CoreJava-telusko/src/UpcastingAndDowncasting.java
@@ -1,11 +1,21 @@
+class UpcastingDemo1 {
+
+    public void show1(){
+        System.out.println("In demo 1 show");
+    }
+}
+
+class UpcastingDemo2 extends UpcastingDemo1{
+    public void show2(){
+        System.out.println("In demo 2 show");
+    }
+}
+
 public class UpcastingAndDowncasting {
     public static void main(String[] args) {
 
-        double d = 4.5;
-        //Type casting from double to int
-        int i = (int) d;
-
-        System.out.println(d);
+        //upcasting subclass to parent class
+        UpcastingDemo1 obj1= (UpcastingDemo1) new UpcastingDemo2();
+        obj1.show1();
     }
-
 }
