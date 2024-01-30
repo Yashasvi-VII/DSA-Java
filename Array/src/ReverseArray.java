1152dcdfdf5061c5ae9d82600e83ab8aef19bed8
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-30 12:51:54 +0530

    Made reverse method static

--- Array/src/ReverseArray.java
+++ Array/src/ReverseArray.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ReverseArray {
 
-    public void reverse(int[] arr, int n) {
+    public static void reverse(int[] arr, int n) {
         int i = 0, j = n - 1;
         while (i <= j) {
             int temp = arr[i];
@@ -14,25 +14,37 @@ public class ReverseArray {
     }
 
     public static void main(String[] args) {
-        System.out.print("Enter size of array :");
+
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array :");
+
         int size = sc.nextInt();
+
         int[] array = new int[size];
+
         System.out.println("Enter elements of array");
+
         for (int i = 0; i < size; i++) {
             array[i] = sc.nextInt();
         }
+
         System.out.print("Elements of array are:- ");
+
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
+
         System.out.println();
-        ReverseArray xyz = new ReverseArray();
-        xyz.reverse(array, size);
+
+        ReverseArray.reverse(array, size);
+
         System.out.print("Elements after reversing array are:- ");
+
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
+
     }
 }
 
